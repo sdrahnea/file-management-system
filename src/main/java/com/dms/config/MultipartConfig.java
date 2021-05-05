36eb89cb0b1bb237c44f8bf53f51941a7ddeb4d5
Sergiu Drahnea <s.drahnea@gmail.com>
2021-05-05 08:38:15 +0300

    FMS-1 fix unable to process parts as no multi-part configuration has been provided

---
+++ src/main/java/com/dms/config/MultipartConfig.java
@@ -0,0 +1,16 @@
+package com.dms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+@Configuration
+public class MultipartConfig {
+
+    @Bean(name = "multipartResolver")
+    public MultipartResolver multipartResolver() {
+        return new CommonsMultipartResolver();
+    }
+
+}
