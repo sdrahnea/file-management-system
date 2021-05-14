476df5dbc9ca4f5e118472404565bb1f34bbff7a
Sergiu Drahnea <s.drahnea@gmail.com>
2021-05-14 17:31:41 +0300

    update data sql file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,4 +3,4 @@ SELECT 1;
 -- insert into constant(name) values ('true'), ('false');
 
 -- INSERT INTO users(username, password) VALUES ('admin', '$2a$10$GIkct28TsCYqOgF4h/KefOVtViBQhhSFdCiyKAmxGYz2tX.JYVEfa');
---TODO add refrences on for entities {{package}} : com.omsentities.history
+--TODO add references on for entities {{package}} : com.omsentities.history
