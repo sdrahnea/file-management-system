f52effc55af810a7d803bc2ae317c0dbe92c9af3
Sergiu Drahnea <s.drahnea@gmail.com>
2021-05-21 19:41:04 +0300

    add storage strategy types

---
+++ src/main/java/com/fms/model/StorageStrategy.java
@@ -0,0 +1,34 @@
+package com.fms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum StorageStrategy {
+
+    /**
+     * File storage rule: ${file.db.location} / ${tenant} / ${file_id}
+     */
+    FILE("FILE"),
+    /**
+     * File storage rule: ${file.db.location} / ${tenant} / ${date} / ${file_id}
+     */
+    FILE_PER_DATE("FILE_PER_DATE"),
+    /**
+     * File storage rule: ${file.db.location} / ${tenant} / ${year} / ${date} /${file_id}
+     */
+    FILE_PER_YEAR_DATE("FILE_PER_YEAR_DATE"),
+    /**
+     * File storage rule: ${file.db.location} / ${tenant} / ${year} / ${month} / ${day} /${file_id}
+     */
+    FILE_PER_YEAR_MONTH_DAY("FILE_PER_YEAR_MONTH_DAY"),
+    /**
+     * File storage rule: ${file.db.location} / ${tenant} / ${year}/ ${month} / ${file_id}
+     */
+    FILE_PER_YEAR_MONTH("FILE_PER_YEAR_MONTH")
+    ;
+
+    private final String name;
+
+}
