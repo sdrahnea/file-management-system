cd9e90f989bd8f799d9ac2a3bb5cfbb7869734d5
Sergiu Drahnea <s.drahnea@gmail.com>
2021-07-06 06:55:08 +0300

    [FMS-8] implement file per year date storage strategy service

--- src/main/java/com/fms/service/storage/FilePerYearDateStorageStrategyServiceService.java
+++ src/main/java/com/fms/service/storage/FilePerYearDateStorageStrategyServiceService.java
@@ -1,16 +1,102 @@
 package com.fms.service.storage;
 
+import com.fms.config.AppConfig;
+import com.fms.model.FileEntity;
 import com.fms.model.StorageDto;
+import com.fms.repository.FileRepository;
+import com.fms.util.DateUtils;
+import com.fms.util.FileUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
+/**
+ * This class contains implementation for file storage rule:
+ *          ${file.db.location} / ${tenant} / ${year} / ${date} /${file_id}
+ * where:
+ *  - ${file.db.location}   the main path to file storage;
+ *  - ${tenant}             an generic identifier
+ *  - ${year}               a folder, which represents the month
+ *  - ${date}               a folder, with yyyy-MM-dd date format
+ *  - ${file_id}            an identifier given by system to content
+ */
+@Slf4j
 @Service
 public class FilePerYearDateStorageStrategyServiceService implements StorageStrategyService {
 
+    private final AppConfig appConfig;
+    private final FileRepository fileRepository;
+
+    @Autowired
+    public FilePerYearDateStorageStrategyServiceService(AppConfig appConfig,
+                                                    FileRepository fileRepository){
+        this.appConfig = appConfig;
+        this.fileRepository = fileRepository;
+    }
+
     @Override
     public Map<String, String> store(StorageDto storageDto) {
-        return null;
+        final String tenant = storageDto.getTenant();
+        final MultipartFile multipartFile = storageDto.getMultipartFile();
+
+        final String fileId = UUID.randomUUID().toString();
+
+        final String filePath = computeAbsoluteFilePath(fileId, tenant);
+
+        checkAndCreateDirectories(filePath);
+
+        saveDocument(fileId, filePath, tenant);
+
+        try {
+            multipartFile.transferTo(new File(filePath));
+        } catch (Exception exception) {
+            log.error("Can not to save file: " + filePath + " exception: " + exception);
+        }
+
+        Map<String, String> map = new HashMap<>();
+        map.put("FILE_ID", fileId);
+        map.put("FILE_PATH", filePath);
+
+        return map;
+    }
+
+    private FileEntity saveDocument(final String fileId,
+                                    final String filePath,
+                                    final String tenant) {
+        FileEntity fileEntity = new FileEntity();
+        fileEntity.setDocumentId(fileId);
+        fileEntity.setPath(filePath);
+        fileEntity.setTenant(tenant);
+
+        log.info("Save to database the file id: {}", fileId);
+
+        return fileRepository.save(fileEntity);
+    }
+
+    private String computeAbsoluteFilePath(final String fileId, final String tenant) {
+        Date date = new Date();
+        return appConfig.getFileDbLocation() + "/" + tenant
+                + "/" + DateUtils.getYear(date)
+                + "/" + DateUtils.formatDate(date)
+                + "/" + fileId;
+    }
+
+    private void checkAndCreateDirectories(final String location) {
+        try {
+            FileUtils.checkAndCreateDirectory(location);
+            Files.createDirectories(Paths.get(location));
+        } catch (Exception exception) {
+            log.error("Could not check or create directory: {}", exception);
+        }
     }
 
 }
--- src/main/java/com/fms/util/DateUtils.java
+++ src/main/java/com/fms/util/DateUtils.java
@@ -6,6 +6,7 @@ import java.util.Date;
 public class DateUtils {
 
     private static final String DATE_FORMAT = "yyyy-MM-dd";
+    private static final String YEAR_DATE_FORMAT = "yyyy";
 
     /**
      * @return current date into DATE_FORMAT value
@@ -13,6 +14,18 @@ public class DateUtils {
     public static String getCurrentDateAsString() {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(DATE_FORMAT);
         return simpleDateFormat.format(new Date());
+    }
+
+    public static String formatDate(final Date date){
+        return new SimpleDateFormat(DATE_FORMAT).format(getCurrentDateIfNull(date));
+    }
+
+    public static String getYear(final Date date) {
+        return new SimpleDateFormat(YEAR_DATE_FORMAT).format(getCurrentDateIfNull(date));
+    }
+
+    private static Date getCurrentDateIfNull(final Date date) {
+        return date == null ? new Date() : date;
     }
 
 }
