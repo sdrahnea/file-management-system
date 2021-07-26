1a9c4f565b8ee1b1aa8c3b0417b9db2a462b0a20
Sergiu Drahnea <s.drahnea@gmail.com>
2021-07-26 08:33:57 +0300

    adjust map helper class

--- src/main/java/com/fms/util/MapHelper.java
+++ src/main/java/com/fms/util/MapHelper.java
@@ -8,6 +8,9 @@ import java.util.Map;
  */
 public class MapHelper {
 
+    public static final String FILE_ID_KEY = "FILE_ID";
+    public static final String FILE_PATH_KEY = "FILE_PATH";
+
     /**
      * Create a map with two keys: FILE_ID and FILE_PATH
      * @param fileId    generated file ID value
@@ -17,8 +20,8 @@ public class MapHelper {
     public static Map<String, String> create(final String fileId,
                                              final String filePath){
         Map<String, String> map = new HashMap<>();
-        map.put("FILE_ID", fileId);
-        map.put("FILE_PATH", filePath);
+        map.put(FILE_ID_KEY, fileId);
+        map.put(FILE_ID_KEY, filePath);
 
         return map;
     }
