fd31a593c6f0261e8efdc572607fdd0cda104ba5
Sergiu Drahnea <s.drahnea@gmail.com>
2021-08-04 07:50:05 +0300

    add a method

--- src/main/java/com/fms/repository/FileRepository.java
+++ src/main/java/com/fms/repository/FileRepository.java
@@ -8,8 +8,10 @@ import java.util.List;
 
 public interface FileRepository extends CrudRepository<FileEntity, Long> {
 
-    //TODO: select should contains the environment
     @Query(name = "select * from document_file where document_id = ?1 limit 1", nativeQuery = true)
     List<FileEntity> findByDocumentId(String documentId);
+
+    @Query(name = "select * from document_file where document_id = ?1 AND tenant = ?1 limit 1", nativeQuery = true)
+    List<FileEntity> findByDocumentIdAndTenant(String documentId, String tenant);
 
 }
