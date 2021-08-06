2bbec74cd596b640a8b2080d9c395ab82e6667dd
Sergiu Drahnea <s.drahnea@gmail.com>
2021-08-06 08:55:13 +0300

    improve tenant verification condition

--- src/main/java/com/fms/service/TenantService.java
+++ src/main/java/com/fms/service/TenantService.java
@@ -1,21 +1,25 @@
 package com.fms.service;
 
 import com.fms.config.AppConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * This class contains implementation related to tenant methods
  */
+@Service
 public class TenantService {
 
     private final AppConfig appConfig;
 
+    @Autowired
     public TenantService(AppConfig appConfig) {
         this.appConfig = appConfig;
     }
 
     public void checkIfTenantIsAllowed(final String tenant){
-        if(!appConfig.getTenantList().contains(tenant)) {
-            new RuntimeException("Tenant is not allowed!");
+        if(appConfig.isTenantVerification() && !appConfig.getTenantList().contains(tenant)) {
+            throw new RuntimeException("Tenant is not allowed!");
         }
     }
 
