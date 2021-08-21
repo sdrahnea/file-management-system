1bac53ebed0f2235c2682b0032d1cb516a964343
Sergiu Drahnea <s.drahnea@gmail.com>
2021-08-21 20:04:44 +0300

    add unit tests for date util class

---
+++ src/test/java/util/DateUtilsTest.java
@@ -0,0 +1,41 @@
+package util;
+
+import com.fms.util.DateUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.Calendar;
+
+public class DateUtilsTest {
+
+    private static final int YEAR = 2021;
+    private static final int MONTH = 8;
+    private static final int DAY = 11;
+
+    static Calendar calendar;
+
+    DateUtils dateUtils;
+
+    @BeforeAll
+    static void init(){
+        calendar = Calendar.getInstance();
+        calendar.set(YEAR, MONTH, DAY);
+    }
+
+    @Test
+    public void should_return_correct_day(){
+       Assertions.assertEquals("" + DAY, dateUtils.getDay(calendar.getTime()));
+    }
+
+    @Test
+    public void should_return_correct_month(){
+        Assertions.assertEquals("09", dateUtils.getMonth(calendar.getTime()));
+    }
+
+    @Test
+    public void should_return_correct_year(){
+        Assertions.assertEquals("" + YEAR, dateUtils.getYear(calendar.getTime()));
+    }
+
+}
