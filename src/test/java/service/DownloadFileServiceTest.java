fb76031361676db3a66cf34d0fc35c91498dac62
Sergiu Drahnea <s.drahnea@gmail.com>
2021-08-31 09:13:23 +0300

    fix download file service test

--- src/test/java/service/DownloadFileServiceTest.java
+++ src/test/java/service/DownloadFileServiceTest.java
@@ -1,14 +1,35 @@
 package service;
 
+import com.fms.model.FileEntity;
 import com.fms.repository.FileRepository;
 import com.fms.service.DownloadFileService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Collections;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
 public class DownloadFileServiceTest {
 
     @Mock
     FileRepository fileRepository;
 
-    DownloadFileService downloadFileService = new DownloadFileService(fileRepository);
+    @InjectMocks
+    DownloadFileService downloadFileService;
+
+    @Test
+    public void test(){
+        FileEntity fileEntity = new FileEntity();
+        List<FileEntity> list = Collections.singletonList(fileEntity);
+        Mockito.when(fileRepository.findByFileId(ArgumentMatchers.anyString())).thenReturn(list);
+
+        downloadFileService.download(ArgumentMatchers.anyString());
+    }
 
 }
