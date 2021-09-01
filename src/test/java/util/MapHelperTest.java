4b19f82d179fe8451fa2132fe756b7f5dddd74f9
Sergiu Drahnea <s.drahnea@gmail.com>
2021-09-01 16:19:39 +0300

    add unit tests for map helper class

---
+++ src/test/java/util/MapHelperTest.java
@@ -0,0 +1,24 @@
+package util;
+
+import com.fms.util.MapHelper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+public class MapHelperTest {
+
+    private static final String FILE_ID = "0123456789";
+    private static final String FILE_PATH = "/opt/java/file.jar";
+
+    MapHelper mapHelper;
+
+    @Test
+    public void should_create_a_map(){
+        Map<String, String> map = mapHelper.create(FILE_ID, FILE_PATH);
+
+        Assertions.assertEquals(FILE_ID, map.get("FILE_ID"));
+        Assertions.assertEquals(FILE_PATH, map.get("FILE_PATH"));
+    }
+
+}
