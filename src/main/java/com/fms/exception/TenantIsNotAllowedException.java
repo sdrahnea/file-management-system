18fb0acc5aa637e86b9d19db4b8b3e53f7b7f99d
Sergiu Drahnea <s.drahnea@gmail.com>
2021-09-08 12:21:51 +0300

    update tenant not allowed exception

--- src/main/java/com/fms/exception/TenantIsNotAllowedException.java
+++ src/main/java/com/fms/exception/TenantIsNotAllowedException.java
@@ -2,8 +2,14 @@ package com.fms.exception;
 
 public class TenantIsNotAllowedException extends RuntimeException {
 
+    private static final String MESSAGE = "Tenant is not allowed!";
+
     public TenantIsNotAllowedException(String message) {
         super(message);
+    }
+
+    public TenantIsNotAllowedException() {
+        super(MESSAGE);
     }
 
 }
