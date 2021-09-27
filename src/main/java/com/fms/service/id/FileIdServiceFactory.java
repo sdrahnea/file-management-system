df5c62e342e18a6b70ede949fd0bcdd2666c8b20
Sergiu Drahnea <s.drahnea@gmail.com>
2021-09-27 08:22:18 +0300

    add file id factory

---
+++ src/main/java/com/fms/service/id/FileIdServiceFactory.java
@@ -0,0 +1,47 @@
+package com.fms.service.id;
+
+import com.fms.exception.MissingFileIdStrategyException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * This class contains implementation to received file id strategy based
+ *      on the provided configuration.
+ */
+@Component
+public class FileIdServiceFactory {
+
+    private final FileIdStrategy fileIdStrategy;
+
+    private final InstantFileIdStrategyService instantFileIdStrategyService;
+    private final UUIDFileIdStrategyService uuidFileIdStrategyService;
+
+    public FileIdServiceFactory(@Value("${file.id.type:UUID}") FileIdStrategy fileIdStrategy,
+                                InstantFileIdStrategyService instantFileIdStrategyService, UUIDFileIdStrategyService uuidFileIdStrategyService) {
+        this.fileIdStrategy = fileIdStrategy;
+        this.instantFileIdStrategyService = instantFileIdStrategyService;
+        this.uuidFileIdStrategyService = uuidFileIdStrategyService;
+    }
+
+    private FileIdStrategyService getIdService() {
+        switch (fileIdStrategy) {
+            case UUID:
+                return uuidFileIdStrategyService;
+            case INSTANT:
+                return instantFileIdStrategyService;
+
+            default:
+                throw new MissingFileIdStrategyException();
+        }
+    }
+
+    /**
+     * Create the file id value based for file id strategy
+     *
+     * @return
+     */
+    public String create() {
+        return getIdService().createId();
+    }
+
+}
