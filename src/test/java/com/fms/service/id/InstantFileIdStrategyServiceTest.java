f628d98d72cdf9c7b3a1d801c1f154551449740b
Sergiu Drahnea <s.drahnea@gmail.com>
2021-10-01 08:22:24 +0300

    add unit test for instabt file id strategy class - fix a test name, divide unit tests

--- src/test/java/com/fms/service/id/InstantFileIdStrategyServiceTest.java
+++ src/test/java/com/fms/service/id/InstantFileIdStrategyServiceTest.java
@@ -11,12 +11,19 @@ public class InstantFileIdStrategyServiceTest {
     InstantFileIdStrategyService instantFileIdStrategyService = new InstantFileIdStrategyService();
 
     @Test
-    public void should_return_instant_value(){
+    public void should_return_a_value_of_a_string_class(){
         Assertions.assertEquals(
                 instantFileIdStrategyService.createId().getClass()
                 , ArgumentMatchers.anyString().getClass())
         ;
 
+        Assertions.assertEquals(true
+                , instantFileIdStrategyService.createId().matches(INTEGER_REGEX)
+        );
+    }
+
+    @Test
+    public void should_match_an_integer_format_regex() {
         Assertions.assertEquals(true
                 , instantFileIdStrategyService.createId().matches(INTEGER_REGEX)
         );
