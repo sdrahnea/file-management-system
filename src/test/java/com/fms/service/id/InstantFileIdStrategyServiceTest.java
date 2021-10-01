5563fed71583cfcee18576470ca67c3d6c61e080
Sergiu Drahnea <s.drahnea@gmail.com>
2021-10-01 08:19:01 +0300

    add unit test for instabt file id strategy class

--- src/test/java/com/fms/service/id/InstantFileIdStrategyServiceTest.java
+++ src/test/java/com/fms/service/id/InstantFileIdStrategyServiceTest.java
@@ -1,4 +1,25 @@
 package com.fms.service.id;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+
 public class InstantFileIdStrategyServiceTest {
+
+    private final static String INTEGER_REGEX = "^\\d+$";
+
+    InstantFileIdStrategyService instantFileIdStrategyService = new InstantFileIdStrategyService();
+
+    @Test
+    public void should_return_instant_value(){
+        Assertions.assertEquals(
+                instantFileIdStrategyService.createId().getClass()
+                , ArgumentMatchers.anyString().getClass())
+        ;
+
+        Assertions.assertEquals(true
+                , instantFileIdStrategyService.createId().matches(INTEGER_REGEX)
+        );
+    }
+
 }
