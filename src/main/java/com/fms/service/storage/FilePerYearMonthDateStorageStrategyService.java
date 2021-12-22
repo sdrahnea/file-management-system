45b43714c2c02ce26d31dc288e0d8960c15dd067
Sergiu Drahnea <s.drahnea@gmail.com>
2021-12-22 07:07:29 +0200

    add file id for ${file.db.location} / ${tenant} / ${year} / ${month} / ${date} / ${file_id} rule

--- src/main/java/com/fms/service/storage/FilePerYearMonthDateStorageStrategyService.java
+++ src/main/java/com/fms/service/storage/FilePerYearMonthDateStorageStrategyService.java
@@ -19,7 +19,7 @@ import java.util.UUID;
 
 /**
  * This class contains implementation for file storage rule:
- *          ${file.db.location} / ${tenant} / ${year} / ${month} / ${date} /${file_id}
+ *          ${file.db.location} / ${tenant} / ${year} / ${month} / ${date} / ${file_id}
  * where:
  *  - ${file.db.location}   the main path to file storage;
  *  - ${tenant}             an generic identifier
@@ -46,7 +46,7 @@ public class FilePerYearMonthDateStorageStrategyService implements StorageStrate
         final String tenant = storageDto.getTenant();
         final MultipartFile multipartFile = storageDto.getMultipartFile();
 
-        final String fileId = UUID.randomUUID().toString();
+        final String fileId = FileUtils.createIdIfNull(storageDto.getFileId());
 
         final String filePath = computeAbsoluteFilePath(fileId, tenant);
 
