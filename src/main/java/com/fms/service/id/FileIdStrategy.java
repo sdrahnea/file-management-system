6eb4cda799972710987930b50dc6fde0109a2bc9
Sergiu Drahnea <s.drahnea@gmail.com>
2022-03-24 14:54:51 +0200

    doc update

--- src/main/java/com/fms/service/id/FileIdStrategy.java
+++ src/main/java/com/fms/service/id/FileIdStrategy.java
@@ -3,6 +3,15 @@ package com.fms.service.id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+/**
+ * This enum is used for file id strategy.
+ * The available file id strategy modes:
+ * 1. UUID      generate an UUID value for file id
+ * 2. INSTANT   now value of instant converted to epoch
+ *
+ * WARN: use INSTANT option in case of low traffic.
+ */
+
 @Getter
 @AllArgsConstructor
 public enum FileIdStrategy {
