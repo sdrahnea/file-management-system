e0127a31290cdb52690c3e01fa321e6285a81afc
Sergiu Drahnea <s.drahnea@gmail.com>
2022-04-20 07:33:16 +0300

    doc update

--- src/main/java/com/fms/service/storage/StorageStrategyService.java
+++ src/main/java/com/fms/service/storage/StorageStrategyService.java
@@ -1,6 +1,6 @@
 package com.fms.service.storage;
 
-import com.fms.model.StorageDto;
+import com.fms.model.StorageDTO;
 
 import java.util.Map;
 
@@ -9,6 +9,6 @@ import java.util.Map;
  */
 public interface StorageStrategyService {
 
-    Map<String, String> store(StorageDto storageDto);
+    Map<String, String> store(StorageDTO storageDto);
 
 }
