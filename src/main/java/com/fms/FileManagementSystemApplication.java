0a8d8cab6b9ac62941442784a9e4c5526a0111bf
Sergiu Drahnea <s.drahnea@gmail.com>
2022-04-24 06:16:32 +0300

    enable scheduling

--- src/main/java/com/fms/FileManagementSystemApplication.java
+++ src/main/java/com/fms/FileManagementSystemApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -15,6 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableJpaRepositories
 @ComponentScan
+@EnableScheduling
 public class FileManagementSystemApplication {
 
     public static void main(String[] args) {
