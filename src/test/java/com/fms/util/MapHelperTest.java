68ddac3aa207583979d8ffd335f39eb342b65532
Sergiu Drahnea <s.drahnea@gmail.com>
2022-04-27 06:29:57 +0300

    fix tests

--- src/test/java/com/fms/util/MapHelperTest.java
+++ src/test/java/com/fms/util/MapHelperTest.java
@@ -1,6 +1,5 @@
 package com.fms.util;
 
-import com.fms.util.MapHelper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -11,11 +10,9 @@ public class MapHelperTest {
     private static final String FILE_ID = "0123456789";
     private static final String FILE_PATH = "/opt/java/file.jar";
 
-    MapHelper mapHelper;
-
     @Test
     public void should_create_a_map(){
-        Map<String, String> map = mapHelper.create(FILE_ID, FILE_PATH);
+        Map<String, String> map = MapHelper.create(FILE_ID, FILE_PATH);
 
         Assertions.assertEquals(FILE_ID, map.get("FILE_ID"));
         Assertions.assertEquals(FILE_PATH, map.get("FILE_PATH"));
